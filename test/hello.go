f849915d70e55d59e202750fd5d38853410e509d
diollia <diollia88@gmail.com>
2024-08-25 21:04:38 +0300

    refactoring with 2 params

--- test/hello.go
+++ test/hello.go
@@ -2,15 +2,43 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello, "
+const helloPrefixEn = "Hello, "
+const helloPrefixDe = "Hallo, "
+const worldEn = "World! "
+const worldDe = "Welt!"
 
 func main() {
-	fmt.Print(Hello("Andrei!"))
+	fmt.Print(Hello("Andrei!", "DE"))
 }
 
-func Hello(name string) string {
-	if name == "" {
-		return helloPrefix + "World!"
+func Hello(name, language string) string {
+	if name == "" && language == "" {
+		return "Hello, World!"
 	}
-	return helloPrefix + name + "!"
+	if name != "" {
+		return getPrefixInLanguage(language) + name + "!"
+
+	} else {
+		return getPrefixInLanguage(language) + getWorldOnLanguage(language)
+	}
+}
+
+func getWorldOnLanguage(language string) string {
+	switch language {
+	case "DE":
+		return worldDe
+	case "EN":
+		return worldEn
+	}
+	return ""
+}
+
+func getPrefixInLanguage(language string) string {
+	switch language {
+	case "DE":
+		return helloPrefixDe
+	case "EN":
+		return helloPrefixEn
+	}
+	return ""
 }
--- test/hello_test.go
+++ test/hello_test.go
@@ -7,12 +7,22 @@ import (
 func TestHello(t *testing.T) {
 	t.Run("If name passed, hello <name> returned", func(t *testing.T) {
 		expected := "Hello, Andrei!"
-		actual := Hello("Andrei")
+		actual := Hello("Andrei", "EN")
 		assertMessage(expected, actual, t)
 	})
-	t.Run("If empty string passed, hello world returned", func(t *testing.T) {
+	t.Run("If empty name passed, hello world returned", func(t *testing.T) {
 		expected := "Hello, World!"
-		actual := Hello("")
+		actual := Hello("", "")
+		assertMessage(expected, actual, t)
+	})
+	t.Run("If language = DE and no name then should see hallo welt", func(t *testing.T) {
+		expected := "Hallo, Welt!"
+		actual := Hello("", "DE")
+		assertMessage(expected, actual, t)
+	})
+	t.Run("If language = DE and name passed then should see hallo <name>", func(t *testing.T) {
+		expected := "Hallo, Andre!"
+		actual := Hello("Andre", "DE")
 		assertMessage(expected, actual, t)
 	})
 }

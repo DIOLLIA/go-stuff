d5796a30031c0a84e260b2d3ff5d2657d04b98e5
andrewRdk <andrei.rudkov@telekom.com>
2024-08-28 18:03:32 +0300

    test_1:  add interfaces stuff rename package table driven tests methods (interfce implementation)

--- test/iterations/repeat.go
+++ test/iterations/repeat.go
@@ -1,7 +1,7 @@
-package iteration
+package iterations
 
-// workout/iteration       1.235s  with Builder 41332561                25.88 ns/op
-// workout/iteration       1.394s  with +=character 11218129               102.0 ns/op
+// workout/iterations       1.235s  with Builder 41332561                25.88 ns/op
+// workout/iterations       1.394s  with +=character 11218129               102.0 ns/op
 func Repeat(seriesCounter int, character string) string {
 	//var builder strings.Builder
 	var result string

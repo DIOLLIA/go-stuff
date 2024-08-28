88b10c805ebfd6624e7fadd3804bfe7888080dc6
andrewRdk <andrei.rudkov@telekom.com>
2024-08-28 14:11:56 +0300

    test_1:  add array&slices tests upd readme

--- test/arrays/array.go
+++ test/arrays/array.go
@@ -7,3 +7,26 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// SumAll takes a varying number of slices, returning a new slice containing the totals for each slice passed in.
+// length and capacity - create array with length 2 (elements initialized by default to 0 for int
+func SumAll(slicesToSum ...[]int) []int {
+	var resultSlice []int
+	for _, collection := range slicesToSum {
+		resultSlice = append(resultSlice, Sum(collection))
+	}
+	return resultSlice
+}
+
+func AppendAllTails(numbersToSum ...[]int) []int {
+	var resultSlice []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			resultSlice = append(resultSlice, 0)
+		} else {
+			tail := numbers[1:]
+			resultSlice = append(resultSlice, Sum(tail))
+		}
+	}
+	return resultSlice
+}
--- test/arrays/array_test.go
+++ test/arrays/array_test.go
@@ -1,6 +1,10 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -13,5 +17,36 @@ func TestSum(t *testing.T) {
 		if result != expected {
 			t.Errorf("Expected %d but got %d, given %v", expected, result, numbers)
 		}
+	})
+}
+func TestSumAll(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		expected := []int{3, 9}
+		result := SumAll([]int{1, 2}, []int{0, 9})
+
+		if !slices.Equal(expected, result) {
+			t.Errorf("expected %v result %v", expected, result)
+		}
+	})
+}
+
+func TestAppendAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, expected, result []int) {
+		t.Helper()
+		if !reflect.DeepEqual(expected, result) {
+			t.Errorf("expected %v result %v", expected, result)
+		}
+	}
+	t.Run("Sum all elements from many to one slice", func(t *testing.T) {
+		result := AppendAllTails([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+
+		checkSums(t, expected, result)
+	})
+	t.Run("Sum elements with zero length slice", func(t *testing.T) {
+		result := AppendAllTails([]int{}, []int{1, 4, 5})
+		expected := []int{0, 9}
+
+		checkSums(t, expected, result)
 	})
 }

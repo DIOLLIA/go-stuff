f4be421a47351b097e483b17945df062703c3fea
andrewRdk <andrei.rudkov@telekom.com>
2024-09-04 12:05:15 +0300

    test_1:  reflection ano structs and funcs 2

--- test/reflection/walker.go
+++ test/reflection/walker.go
@@ -2,18 +2,41 @@ package reflection
 
 import "reflect"
 
-func walk(x interface{}, fn func(string)) {
+func walk(x interface{}, fn func(input string)) {
+	value := getValue(x)
+
+	switch value.Kind() {
+	case reflect.Struct:
+		for i := 0; i < value.NumField(); i++ {
+			walk(value.Field(i).Interface(), fn)
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < value.Len(); i++ {
+			walk(value.Index(i).Interface(), fn)
+		}
+	case reflect.Map:
+		for _, key := range value.MapKeys() {
+			walk(value.MapIndex(key).Interface(), fn)
+		}
+	case reflect.Chan:
+		for v, ok := value.Recv(); ok; v, ok = value.Recv() {
+			walk(v.Interface(), fn)
+		}
+	case reflect.Func:
+		results := value.Call(nil)
+		for _, res := range results {
+			walk(res.Interface(), fn)
+		}
+
+	case reflect.String:
+		fn(value.String())
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
 	value := reflect.ValueOf(x)
 	if value.Kind() == reflect.Pointer {
-		value = value.Elem()
+		value = value.Elem() //to get the value of the pointer
 	}
-	for i := 0; i < value.NumField(); i++ {
-		field := value.Field(i)
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
-	}
+	return value
 }
--- test/reflection/walker_test.go
+++ test/reflection/walker_test.go
@@ -5,15 +5,16 @@ import (
 	"testing"
 )
 
+type Profile struct {
+	Age  int
+	City string
+}
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
 func TestWalk(t *testing.T) {
-	type Profile struct {
-		Age  int
-		City string
-	}
-	type Person struct {
-		Name    string
-		Profile Profile
-	}
 
 	cases := []struct {
 		Name           string
@@ -22,12 +23,12 @@ func TestWalk(t *testing.T) {
 	}{
 		{
 			Name:           "Struct with one string field",
-			Input:          struct{ input string }{"Andrei"},
+			Input:          struct{ Input string }{"Andrei"},
 			ExpectedResult: []string{"Andrei"},
 		},
 		{
 			Name:           "Struct with two string fields",
-			Input:          struct{ input1, input2 string }{"Andrei", "Lucy"},
+			Input:          struct{ Input1, Input2 string }{"Andrei", "Lucy"},
 			ExpectedResult: []string{"Andrei", "Lucy"},
 		},
 		{
@@ -58,18 +59,122 @@ func TestWalk(t *testing.T) {
 			},
 			ExpectedResult: []string{"Andrei", "Spb"},
 		},
+		{
+			Name: "slices",
+			Input: []Profile{
+				{
+					City: "Spb",
+					Age:  78},
+				{
+					City: "Krsk",
+					Age:  24},
+			},
+			ExpectedResult: []string{"Spb", "Krsk"},
+		},
+		{
+			Name: "arrays",
+			Input: [2]Profile{
+				{
+					City: "Spb",
+					Age:  78},
+				{
+					City: "Krsk",
+					Age:  24},
+			},
+			ExpectedResult: []string{"Spb", "Krsk"},
+		},
 	}
 
 	for _, test := range cases {
 		t.Run(test.Name, func(t *testing.T) {
 			var result []string
 
-			walk(test.Input, func(input string) {
-				result = append(result, input)
+			walk(test.Input, func(Input string) {
+				result = append(result, Input)
 			})
 			if !reflect.DeepEqual(test.ExpectedResult, result) {
 				t.Errorf("expected %q, got %q", test.ExpectedResult, result)
 			}
 		})
+	}
+	t.Run("with maps", func(t *testing.T) {
+		cities := map[string]string{
+			"City":    "Spb",
+			"Town":    "Krsk",
+			"Village": "Jelik"}
+
+		var result []string
+		walk(cities, func(input string) {
+			result = append(result, input)
+		})
+		assertContains(t, "Spb", result)
+		assertContains(t, "Krsk", result)
+		assertContains(t, "Jelik", result)
+	})
+}
+
+func TestWalkWithChannels(t *testing.T) {
+	walkChan := make(chan Profile)
+	go func() {
+		walkChan <- Profile{22, "Jelik"}
+		walkChan <- Profile{24, "Krsk"}
+		close(walkChan)
+	}()
+
+	expected := []string{"Jelik", "Krsk"}
+	var result []string
+
+	walk(walkChan, func(input string) {
+		result = append(result, input)
+	})
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("expected %q, got %q", expected, result)
+	}
+}
+
+func TestWalkFunc(t *testing.T) {
+	walkFunc := func() (Profile, Profile) {
+		return Profile{22, "Jelik"}, Profile{24, "Krsk"}
+	}
+
+	expected := []string{"Jelik", "Krsk"}
+	var result []string
+
+	walk(walkFunc, func(input string) {
+		result = append(result, input)
+	})
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("expected %q, got result %q", expected, result)
+	}
+}
+
+func assertContains(t testing.TB, expected string, result []string) {
+	t.Helper()
+	contains := false
+	for _, x := range result {
+		if x == expected {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %v to contain %q but it didn't", expected, result)
+	}
+}
+
+func TestWalkInitial(t *testing.T) {
+
+	expected := "Chris"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
 	}
 }
